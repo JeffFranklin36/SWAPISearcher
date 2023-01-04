ed05ee5daa4d5e85c04a9b8304022b91c9b95d14
Jeffrey <jeff.1001@live.com>
2023-01-04 13:48:33 -0600

    background color fixed

--- src/Components/StarshipSearch.js
+++ src/Components/StarshipSearch.js
@@ -21,7 +21,7 @@ const StarshipSearch = () => {
   };
 
   return (
-    <div>
+   <div className='body'>
       <form className='search' onSubmit={handleSearch}>
         <div className='box'>
           <input
