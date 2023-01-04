98b7de33959cb07e43f6d5017c478adb3e50614b
Jeffrey <jeff.1001@live.com>
2023-01-04 12:56:51 -0600

    navbar and search results responsive

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,22 +1,37 @@
-import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import './nav.css'
+import logo from '../images/logo.png'
 
-const NavBar = () => (
-  <Navbar bg="dark" variant="dark" className="navbar">
-   <Container>
-      <Nav className="me-auto">
-        <Nav.Link  href="/">Home</Nav.Link>
-        <Nav.Link  href="/planets">Planets</Nav.Link>
-        <Nav.Link  href="/people">People</Nav.Link>
-        <Nav.Link  href="/species">Species</Nav.Link>
-        <Nav.Link  href="/starships">Starships</Nav.Link>
-        <Nav.Link  href="/vehicles">Vehicles</Nav.Link>
-      </Nav>
+function NavBar() {
+  return (
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand className='brand' href="/">
+          <img src={logo} alt="crossed lightsabers"/>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="ml-auto">
+            <Nav.Link  href="/planets">Planets</Nav.Link>
+            <Nav.Link  href="/people">People</Nav.Link>
+            <Nav.Link  href="/species">Species</Nav.Link>
+            <Nav.Link  href="/starships">Starships</Nav.Link>
+            <Nav.Link  href="/vehicles">Vehicles</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
       </Container>
-  </Navbar>
-);
+    </Navbar>
+  );
+}
 
 export default NavBar;
+
+
+
+
+
+
+
+
