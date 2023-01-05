86933e43e7f25cd7767db8a6e2ae85703ad426bf
Jeffrey <jeff.1001@live.com>
2023-01-05 15:11:24 -0600

    bootstrap grid format implemented

--- src/Components/PeopleSearch.js
+++ src/Components/PeopleSearch.js
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
-import './results.css'
+import './results.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
-import { Card } from 'react-bootstrap';
-import logo from '../images/notFound.webp'
+import logo from '../images/notFound.webp';
+import { Container, Row, Col, Card } from 'react-bootstrap';
 
 const PeopleSearch = () => {
   const [query, setQuery] = useState('');
@@ -20,32 +20,42 @@ const PeopleSearch = () => {
       });
   };
 
-
   return (
-    <div className='body'>
-      <form className='search' onSubmit={handleSearch}>
-        <div className='box'>
-          <input
-            type="text"
-            id="query"
-            placeholder='Search People'
-            className='input'
-            value={query}
-            onChange={event => setQuery(event.target.value)}
-          />
-          <button type="submit" className="button">
-            <FontAwesomeIcon icon={faSearch} />
-          </button>
-        </div>
-      </form>
+    <Container>
+      <Row>
+        <Col xs={12} md={4} className='w-100'>
+          <form className='search' onSubmit={handleSearch}>
+            <input
+              type="text"
+              id="query"
+              placeholder='Search People'
+              className='input w-75'
+              value={query}
+              onChange={event => setQuery(event.target.value)}
+            />
+            <button type="submit" className="button w-25">
+              <FontAwesomeIcon icon={faSearch} />
+            </button>
+          </form>
+        </Col>
+      </Row>
       {searched && results.length === 0 ? (
-          <div className='notFound'>
-           <img src={logo} alt="No Results Found" />
-          </div>
-           ) : (
-          <div className="grid-container">
-            {results.slice(0, 9).map(result => (
-              <Card key={result.name} className="card">
+      <Row>
+        <Col xs={12} md={4}  className='notFound'>
+          <Card className='w-100 h-100'>
+              <Card.Img className='w-100 h-100' variant="top" src={logo} alt="No Results Found" />
+                <Card.Body>
+                  <Card.Title>No Results Found</Card.Title>
+                  <Card.Text>Please try again with a different search term.</Card.Text>
+                </Card.Body>
+            </Card>
+        </Col>
+      </Row>
+  ) : (
+        <Row>
+          {results.slice(0, 9).map(result => (
+            <Col className='container' xs={12} md={4} key={result.name}>
+              <Card className="card">
                 <Card.Body>
                   <Card.Title>{result.name}</Card.Title>
                   <Card.Text>
@@ -55,12 +65,59 @@ const PeopleSearch = () => {
                   </Card.Text>
                 </Card.Body>
               </Card>
-            ))}
-          </div>
-        )}
-    </div>
+            </Col>
+          ))}
+        </Row>
+      )}
+    </Container>
   );
-}
+};
 
-export default PeopleSearch;
+  export default PeopleSearch;
+
+//   return (
+//     <div>
+//       <div className='row'>
+//         <div className='col-indentend-4'></div>
+//       </div>
+//       <form className='search' onSubmit={handleSearch}>
+//         <div className='box'>
+//           <input
+//             type="text"
+//             id="query"
+//             placeholder='Search People'
+//             className='input'
+//             value={query}
+//             onChange={event => setQuery(event.target.value)}
+//           />
+//           <button type="submit" className="button">
+//             <FontAwesomeIcon icon={faSearch} />
+//           </button>
+//         </div>
+//       </form>
+//       {searched && results.length === 0 ? (
+//           <div className='notFound'>
+//            <img src={logo} alt="No Results Found" />
+//           </div>
+//            ) : (
+//           <div className="grid-container">
+//             {results.slice(0, 9).map(result => (
+//               <Card key={result.name} className="card">
+//                 <Card.Body>
+//                   <Card.Title>{result.name}</Card.Title>
+//                   <Card.Text>
+//                     <p>Gender: {result.gender}</p>
+//                     <p>Weight: {result.mass} Kilograms</p>
+//                     <p>Height: {result.height} Meters</p>
+//                   </Card.Text>
+//                 </Card.Body>
+//               </Card>
+//             ))}
+//           </div>
+//         )}
+//     </div>
+//   );
+// }
+
+
 
--- src/Components/VehicleSearch.js
+++ src/Components/VehicleSearch.js
@@ -2,8 +2,8 @@ import React, { useState} from 'react';
 import './results.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
-import { Card } from 'react-bootstrap';
 import logo from '../images/notFound.webp'
+import { Container, Row, Col, Card } from 'react-bootstrap';
 
 const VehicleSearch = () => {
   const [query, setQuery] = useState('');
@@ -21,44 +21,58 @@ const VehicleSearch = () => {
   };
 
   return (
-    <div className='body'>
-      <form className='search' onSubmit={handleSearch}>
-        <div className='box'>
-          <input
-            type="text"
-            id="query"
-            placeholder='Search Vehicles'
-            className='input'
-            value={query}
-            onChange={event => setQuery(event.target.value)}
-          />
-          <button type="submit" className="button">
-            <FontAwesomeIcon icon={faSearch} />
-          </button>
-        </div>
-      </form>
+    <Container>
+      <Row>
+        <Col xs={12} md={4} className='w-100'>
+          <form className='search' onSubmit={handleSearch}>
+            <div className='box'>
+              <input
+                type="text"
+                id="query"
+                placeholder='Search Vehicles'
+                className='input w-75'
+                value={query}
+                onChange={event => setQuery(event.target.value)}
+              />
+              <button type="submit" className="button w-25">
+                <FontAwesomeIcon icon={faSearch} />
+              </button>
+            </div>
+          </form>
+        </Col>
+      </Row>
       {searched && results.length === 0 ? (
-          <div className='notFound'>
-           <img src={logo} alt="No Results Found" />
-          </div>
-      ) : (
-          <div className="grid-container">
-            {results.slice(0, 9).map(result => (
-              <Card key={result.name} className="card">
-                <Card.Body>
-                <Card.Title>{result.name}</Card.Title>
-                  <Card.Text>
-                    <p>Manufacturer: {result.manufacturer}</p>
-                    <p>Model: {result.model}</p>
-                    <p>Cost: {result.cost_in_credits} Credits</p>
-                    <p>Capacity: {result.crew} crewmen & {result.passengers} passengers</p>
-                  </Card.Text>
-                </Card.Body>
+        <Row>
+          <Col xs={12} md={4}  className='notFound'>
+            <Card className='w-100 h-100'>
+                <Card.Img className='w-100 h-100' variant="top" src={logo} alt="No Results Found" />
+                  <Card.Body>
+                    <Card.Title>No Results Found</Card.Title>
+                    <Card.Text>Please try again with a different search term.</Card.Text>
+                  </Card.Body>
               </Card>
+          </Col>
+        </Row>
+      ) : (
+          <Row>
+            {results.slice(0, 9).map(result => (
+              <Col className='container' xs={12} md={4} key={result.name}>
+                <Card key={result.name} className="card">
+                  <Card.Body>
+                    <Card.Title>{result.name}</Card.Title>
+                    <Card.Text>
+                      <p>Manufacturer: {result.manufacturer}</p>
+                      <p>Model: {result.model}</p>
+                      <p>Cost: {result.cost_in_credits} Credits</p>
+                      <p>Capacity: {result.crew} crewmen & {result.passengers} passengers</p>
+                    </Card.Text>
+                  </Card.Body>
+                </Card>
+              </Col>
             ))}
-          </div>
+          </Row>
     )}
-    </div>
+    </Container>
   );
 }
 
