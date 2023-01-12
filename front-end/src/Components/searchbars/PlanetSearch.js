eedc30e9a5c23a89bb7753c92299a6a5383a4de7
Jeffrey <jeff.1001@live.com>
2023-01-11 18:29:26 -0600

    planet search cleaned up

--- front-end/src/Components/searchbars/PlanetSearch.js
+++ front-end/src/Components/searchbars/PlanetSearch.js
@@ -20,28 +20,33 @@ const PlanetSearch = () => {
   const [searched, setSearched] = useState(false);
   const [page, setPage] = useState(1);
   const [totalPages, setTotalPages] = useState(null);
+  const [searchClicked, setSearchClicked] = useState(false);
 
-  const handleSearch = useCallback((event) => {
+  const handleSearch = useCallback(async (event) => {
     if (event) {
       event.preventDefault();
     }
-    setPage(page);
-    console.log('searching')
-    axios.get(`https://swapi.dev/api/planets?search=${query}&page=${page}`)
-      .then(response => {
+    if(query.length >= -1) {
+      setPage(page);
+      console.log('searching planets', query)
+      try {
+        const response = await axios.get(`https://swapi.dev/api/planets?search=${query}&page=${page}`)
+        console.log(response.data.results)
         setResults(response.data.results);
         setTotalPages(Math.ceil(response.data.count / 10));
         setSearched(true);
-      })
-      .catch(error => {
+      } catch (error) {
         console.error(error);
-      });
+      }
+      console.log(page)
+      console.log(query.length)
+    }
   }, [query, page]);
   
-  
   useEffect(() => {
-    handleSearch();
-  }, [handleSearch, page]);
+    if(searchClicked) handleSearch();
+  }, [handleSearch, page, searchClicked]);
+  
   
   return (
     <Container>
@@ -50,7 +55,7 @@ const PlanetSearch = () => {
       </Row>
       <Row>
         <Col xs={12} md={4} className='w-100'>
-          <form className='search' onSubmit={event => handleSearch(event)}>
+        <form className='search' onSubmit={event => {event.preventDefault();setSearchClicked(true)}}>
             <div className='box'>
               <input
                 type="text"
@@ -58,7 +63,7 @@ const PlanetSearch = () => {
                 placeholder='Search Planets'
                 className='input w-75'
                 value={query}
-                onChange={event => setQuery(event.target.value)}
+                onChange={event => {setQuery(event.target.value);setPage(1)} }
               />
               <button type="submit" className="button w-25">
                 <FontAwesomeIcon icon={faSearch} />
@@ -74,6 +79,70 @@ const PlanetSearch = () => {
       )}
     </Container>
   );
-}
+};
 
-export default PlanetSearch;
+  export default PlanetSearch;
+
+// const PlanetSearch = () => {
+//   const [query, setQuery] = useState('');
+//   const [results, setResults] = useState([]);
+//   const [searched, setSearched] = useState(false);
+//   const [page, setPage] = useState(1);
+//   const [totalPages, setTotalPages] = useState(null);
+//   const [searchClicked, setSearchClicked] = useState(false);
+
+//   const handleSearch = useCallback((event) => {
+//     if (event) {
+//       event.preventDefault();
+//     }
+//     setPage(page);
+//     axios.get(`https://swapi.dev/api/planets?search=${query}&page=${page}`)
+//       .then(response => {
+//         setResults(response.data.results);
+//         setTotalPages(Math.ceil(response.data.count / 10));
+//         setSearched(true);
+//       })
+//       .catch(error => {
+//         console.error(error);
+//       });
+//   }, [query, page]);
+  
+  
+//   useEffect(() => {
+//     handleSearch();
+//   }, [handleSearch, page]);
+  
+//   return (
+//     <Container>
+//       <Row className='banner'>
+//         <img src={banner} alt=""/>
+//       </Row>
+//       <Row>
+//         <Col xs={12} md={4} className='w-100'>
+//           <form className='search' onSubmit={event => handleSearch(event)}>
+//             <div className='box'>
+//               <input
+//                 type="text"
+//                 id="query"
+//                 placeholder='Search Planets'
+//                 className='input w-75'
+//                 value={query}
+//                 onChange={event => setQuery(event.target.value)}
+//               />
+//               <button type="submit" className="button w-25">
+//                 <FontAwesomeIcon icon={faSearch} />
+//               </button>
+//             </div>
+//           </form>
+//         </Col>
+//       </Row>
+//       {searched && results && <PlanetResults results={results} />}
+//       {searched && !results.length && <NoResults results={results}/>}
+//       {searched && results && totalPages > 1 && (
+//         <Pagination handleSearch={handleSearch} page={page} setPage={setPage} totalPages={totalPages}/>
+//       )}
+//     </Container>
+//   );
+// }
+
+// export default PlanetSearch;
